baccf053d00b37f7dc9e5ee62543df40f964033b
Homa rugvedha chappidi <homarugvedha@gmail.com>
2026-08-06 22:05:46 +0530

    Update test_api.py

--- scraper/test_api.py
+++ scraper/test_api.py
@@ -1,1 +1,20 @@
+import requests
+import json
 
+url = "https://extranet-lv.bwfbadminton.com/api/tournaments/draw/players"
+
+params = {
+    "tournament_id": 4592,
+    "draw_code": 5
+}
+
+response = requests.get(url, params=params)
+
+print("Status Code:", response.status_code)
+
+try:
+    data = response.json()
+    print(json.dumps(data, indent=4))
+except Exception as e:
+    print("Error:", e)
+    print(response.text)
